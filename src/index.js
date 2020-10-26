fd19bc21cf9e1e1cdc022803e3de96c92efb5a10
sbdnd <sebastien.dunand@gmail.com>
2020-10-26 10:06:26 +0100

    ajouter une todo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import "./style.css";
 //récupération d'une référence à la balise <ul>
 const ul = document.querySelector("ul");
 
+//récupération d'une référence à la balise <form>
+const form = document.querySelector("form");
+
+//récupération d'une référence à la balise <input>
+const input = document.querySelector("input");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = input.value;
+    input.value = '';
+    addTodo(value);
+    displayTodo();
+});
+
+
 const todos = [
     {
         text: "je suis une todo non validée",
@@ -14,6 +29,7 @@ const todos = [
        done: true
     }
 ]
+
 
 const displayTodo = () => {
     const todosNode = todos.map((todo, index) =>{
@@ -32,5 +48,12 @@ const createTodoElement = (todo, index) => {
     `;
     return li;
 };
+
+const addTodo = (text) => {
+    todos.push({
+        text,
+        done: false
+    });
+}
 
 displayTodo();
