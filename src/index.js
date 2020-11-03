7f9def0f7c0cfc6031d1ecb6f1b3fafe349a999e
sbdnd <sebastien.dunand@gmail.com>
2020-11-03 18:05:03 +0100

    edit button

--- src/index.js
+++ src/index.js
@@ -15,12 +15,14 @@ const input = document.querySelector("input");
 const todos = [
     {
         text: "je suis une todo non validée",
-        done: false
+        done: false,
+        editMode: true
     },
     
     {
         text: "je suis une todo validée",
-        done: true
+        done: true,
+        editMode: false
     }
 ];
 
@@ -40,37 +42,72 @@ form.addEventListener("submit", e => {
  */
 const displayTodo = () => {
     const todosNode = todos.map((todo, index) =>{
-        return createTodoElement(todo, index);
+        if(todo.editMode){
+            return createTodoEditElement(todo, index);
+        } else {
+            return createTodoElement(todo, index);
+        }
     });
     ul.innerHTML = '';
     ul.append(...todosNode);
 };
 
 /**
- * Créé un bouton supprimer, l'ajoute à la <li>, supprime une todo en fonctionn de son index
- * Créé un todo, lui le contenu : <span>, text et bouton supprimer
+ * Créé un bouton supprimer, l'ajoute à la <li>, supprime une todo en fonction de son index
+ * Créé une todo avec bouton toggle, text et bouton supprimer
  * @param {*} todo 
  * @param {number} index 
  */
 const createTodoElement = (todo, index) => {
     const li = document.createElement("li");
     const buttonDelete = document.createElement('button');
-    buttonDelete.innerText = "Supprimer";
-    buttonDelete.addEventListener('click', e=>{
+    buttonDelete.innerText = "Delete";
+    const buttonEdit = document.createElement('button'); 
+    buttonEdit.innerText = "Edit";
+    buttonDelete.addEventListener('click', e =>{
         e.stopPropagation();
         deleteTodo(index);
     });
+    buttonEdit.addEventListener('click', e =>{
+        e.stopPropagation();
+        toggleEditMode(index);
+    });
+
     li.innerHTML = `
     <span class="todo ${ todo.done ? 'done' : '' }"></span>
     <p>${ todo.text }</p>
     `;
-    li.appendChild(buttonDelete);
+    li.append(buttonEdit, buttonDelete);
     li.addEventListener('click', e=>{
         toggleTodo(index);
     })
     return li;
-    
 };
+
+/**
+ * Créé une todo sou forme d'input et deux boutons (cancel et save)
+ * @param {*} todo 
+ * @param {number} index 
+ */
+const createTodoEditElement = (todo, index) => {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = todo.text;
+    const buttonSave = document.createElement('button');
+    buttonSave.innerHTML = "Save";
+    buttonSave.addEventListener('click', e=>{
+        editTodo(index, input);
+    })
+    const buttonCancel = document.createElement('button')
+    buttonCancel.innerHTML = "Cancel";
+    buttonCancel.addEventListener('click', e=>{
+        e.stopPropagation();
+        toggleEditMode(index);
+    })
+    li.append(input, buttonSave,buttonCancel);
+    return li;
+}
 
 /**
  * Ajoute une todo au tableau Todos
@@ -79,7 +116,8 @@ const createTodoElement = (todo, index) => {
 const addTodo = (text) => {
     todos.push({
         text,
-        done: false
+        done: false,
+        editMode: false
     });
     displayTodo();
 };
@@ -96,6 +134,19 @@ const deleteTodo = index => {
 const toggleTodo = index => {
     todos[index].done = !todos[index].done;
     displayTodo();
+}
+
+const toggleEditMode = index => {
+    todos[index].editMode = !todos[index].editMode;
+    displayTodo();
+}
+
+const editTodo = (index, input) => {
+    const value = input.value;
+    todos[index].text = value;
+    todos[index].editMode = false;
+    displayTodo();
+
 }
 
 displayTodo();
